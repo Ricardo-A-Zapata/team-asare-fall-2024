629cbac4178c16d7648d96d3f28db76133563ff6
Ricardo-A-Zapata <raz6675@nyu.edu>
2024-10-18 02:30:58 -0400

    Created test_update()
    Created test_update() in data/tests/test_text.py in order to test text.py for update functionality
    
    Summary:
         - Created test_update() to test update functionality used in text.py

--- data/tests/test_text.py
+++ data/tests/test_text.py
@@ -18,8 +18,21 @@ def test_read_one():
     assert len(txt.read_one(txt.TEST_KEY)) > 0
 
 
+BEFORE_TITLE = 'Old Title'
+AFTER_TITLE = 'Updated Title'
+UPDATE_KEY = 'update_key'
+UPDATE_TEXT = 'Updated text content'
+
 def test_update():
-    pass
+    txt.create(UPDATE_KEY, BEFORE_TITLE, 'Old text content')
+    texts = txt.read()
+    assert UPDATE_KEY in texts
+    assert BEFORE_TITLE == texts[UPDATE_KEY][txt.TITLE]
+
+    txt.update(UPDATE_KEY, AFTER_TITLE, UPDATE_TEXT)
+    texts = txt.read()
+    assert texts[UPDATE_KEY][txt.TITLE] == AFTER_TITLE
+    assert texts[UPDATE_KEY][txt.TEXT] == UPDATE_TEXT
 
 
 def test_delete():
