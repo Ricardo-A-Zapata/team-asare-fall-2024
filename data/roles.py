6c10239cbf485c9c54a73bbda3eb2568c3b19a13
Elisha-Edme <eae8374@nyu.edu>
2024-10-24 07:08:00 -0400

    created roles.py

---
+++ data/roles.py
@@ -0,0 +1,16 @@
+"""
+This module manages person roles for a journal.
+"""
+AUTHOR_CODE = 'AU'
+EDITOR_CODE = 'ED'
+REFEREE_CODE = 'RE'
+TEST_CODE = AUTHOR_CODE
+ROLES = {
+    AUTHOR_CODE: 'Author',
+    EDITOR_CODE: 'Editor',
+    REFEREE_CODE: 'Referee',
+}
+def get_roles() -> dict:
+    return ROLES
+def is_valid(code: str) -> bool:
+    return code in ROLES
