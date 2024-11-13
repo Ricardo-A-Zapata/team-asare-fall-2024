b09bc47d0344e6fa0d4481ad26fa496cb77e3dc5
SamVotr <sam@govotr.com>
2024-11-13 18:59:23 -0500

    Create manuscripts data model
    Created the manuscripts data model. Will be iterated on later in order to connect to mongodb

---
+++ data/manuscripts.py
@@ -0,0 +1,68 @@
+"""
+This file contains the manuscript data and operations.
+"""
+
+from typing import Dict, List, Optional
+from datetime import datetime
+
+# Constants for manuscript fields
+TITLE = 'title'
+AUTHOR = 'author'
+AUTHOR_EMAIL = 'author_email'
+STATE = 'state'
+REFEREES = 'referees'
+TEXT = 'text'
+ABSTRACT = 'abstract'
+HISTORY = 'history'
+EDITOR = 'editor'
+
+# Constants for manuscript states
+STATE_SUBMITTED = 'SUBMITTED'
+STATE_REFEREE_REVIEW = 'REFEREE_REVIEW'
+STATE_REJECTED = 'REJECTED'
+STATE_AUTHOR_REVISIONS = 'AUTHOR_REVISIONS'
+STATE_EDITOR_REVIEW = 'EDITOR_REVIEW'
+STATE_COPY_EDIT = 'COPY_EDIT'
+STATE_AUTHOR_REVIEW = 'AUTHOR_REVIEW'
+STATE_FORMATTING = 'FORMATTING'
+STATE_PUBLISHED = 'PUBLISHED'
+STATE_WITHDRAWN = 'WITHDRAWN'
+
+# Constants for referee verdicts
+VERDICT_ACCEPT = 'ACCEPT'
+VERDICT_ACCEPT_WITH_REVISIONS = 'ACCEPT_W_REV'
+VERDICT_REJECT = 'REJECT'
+
+# In-memory storage (will be replaced with MongoDB)
+manuscripts: Dict = {}
+
+def create_manuscript(
+    title: str,
+    author: str,
+    author_email: str,
+    text: str,
+    abstract: str
+) -> dict:
+    """
+    Create a new manuscript entry.
+    """
+    timestamp = datetime.now().isoformat()
+    manuscript = {
+        TITLE: title,
+        AUTHOR: author,
+        AUTHOR_EMAIL: author_email,
+        STATE: STATE_SUBMITTED,
+        REFEREES: {},
+        TEXT: text,
+        ABSTRACT: abstract,
+        HISTORY: [{
+            'state': STATE_SUBMITTED,
+            'timestamp': timestamp,
+            'actor': author_email
+        }],
+        EDITOR: None
+    }
+    
+    # For now, use timestamp as ID (will be replaced with MongoDB _id)
+    manuscripts[timestamp] = manuscript
+    return manuscript
