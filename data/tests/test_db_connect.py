b8f2adc4cbed714cef185207277d1d476efc23f3
Ricardo-A-Zapata <raz6675@nyu.edu>
2024-11-14 22:33:27 -0500

    Added tests for fetch from db_connect.py
    Added test_fetch_one() and test_fetch_one_nonexistent_document() for fetch from db_connect.py. Used mock_mongo fixture to ensure encapsulated testing
    
    Summary
       - Created test_fetch_one(mock_mongo)
       - Created test_fetch_one_nonexistent_document(mock_mongo)

--- data/tests/test_db_connect.py
+++ data/tests/test_db_connect.py
@@ -14,3 +14,16 @@ def test_insert_one(mock_mongo):
     doc = {"name": "test", "value": 123}
     result = db.insert_one(collection, doc)
     assert result.inserted_id is not None
+
+def test_fetch_one(mock_mongo):
+    collection = "TEST"
+    doc = {"name": "test", "value": 123}
+    db.insert_one(collection, doc)
+    result = db.fetch_one(collection, {"name": "test"})
+    assert result["name"] == "test"
+    assert result["value"] == 123
+
+def test_fetch_one_nonexistent_document(mock_mongo):
+    collection = "test_collection"
+    result = db.fetch_one(collection, {"name": "Nonexistent"})
+    assert result is None
