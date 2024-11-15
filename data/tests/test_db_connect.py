f257d3f42c4f0d17cecfca80474f78c5f3175b89
Ricardo-A-Zapata <raz6675@nyu.edu>
2024-11-14 22:50:35 -0500

    Added test for fetch and added global variables
    Added test_fetch_one_with_duplicates(mock_mongo) for fetch and added global variables in test_db_connect.py.
    
    Summary
       - Made commonly used variables for tests
       - Added test_fetch_one_with_duplicates(mock_mongo)

--- data/tests/test_db_connect.py
+++ data/tests/test_db_connect.py
@@ -3,6 +3,11 @@ from unittest import mock
 import mongomock
 import data.db_connect as db
 
+TEST_COLLECTION = "TEST"
+TEST_DOC = {"TEST_NAME": "TEST", "TEST_VALUE": 123}
+TEST_FILT = {"TEST_NAME": "TEST"}
+TEST_NONEXISTENT_FILT = {"TEST_NAME": "DOES_NOT_EXIST"}
+
 @pytest.fixture
 def mock_mongo():
     with mock.patch("pymongo.MongoClient", return_value=mongomock.MongoClient()):
@@ -10,20 +15,26 @@ def mock_mongo():
         yield db.client
 
 def test_insert_one(mock_mongo):
-    collection = "TEST"
-    doc = {"name": "test", "value": 123}
-    result = db.insert_one(collection, doc)
+    result = db.insert_one(TEST_COLLECTION, TEST_DOC)
     assert result.inserted_id is not None
 
 def test_fetch_one(mock_mongo):
-    collection = "TEST"
-    doc = {"name": "test", "value": 123}
-    db.insert_one(collection, doc)
-    result = db.fetch_one(collection, {"name": "test"})
-    assert result["name"] == "test"
-    assert result["value"] == 123
+    db.insert_one(TEST_COLLECTION, {"_id": 1, "TEST_NAME": "TEST", "TEST_VALUE": 123})
+    result = db.fetch_one(TEST_COLLECTION, TEST_FILT)
+    assert result["TEST_NAME"] == "TEST"
+    assert result["TEST_VALUE"] == 123
+
 
 def test_fetch_one_nonexistent_document(mock_mongo):
-    collection = "test_collection"
-    result = db.fetch_one(collection, {"name": "Nonexistent"})
+    result = db.fetch_one(TEST_COLLECTION, TEST_NONEXISTENT_FILT)
     assert result is None
+
+def test_fetch_one_with_duplicates(mock_mongo):
+    db.insert_one(TEST_COLLECTION, {"TEST_NAME": "TEST", "TEST_VALUE": 123})
+    db.insert_one(TEST_COLLECTION, {"TEST_NAME": "TEST", "TEST_VALUE": 1234})
+
+    result = db.fetch_one(TEST_COLLECTION, TEST_FILT)
+
+    assert result is not None
+    assert result["TEST_NAME"] == "TEST"
+    assert result["TEST_VALUE"] == 123
